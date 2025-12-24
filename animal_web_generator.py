ee3030a0e00311b99b3007b86cdf991bba46bc25
Houssin <el-hb@outlook.de>
2025-12-24 20:49:36 +0100

    verbessere HTML der Karten-Elemente

--- animal_web_generator.py
+++ animal_web_generator.py
@@ -14,29 +14,32 @@ def main():
         output += '<li class="cards__item">\n'
 
         if 'name' in animal:
-            output += f"    Name: {animal['name']}<br/>\n"
+            output += f'  <div class="card__title">{animal["name"]}</div>\n'
+
+        output += '  <p class="card__text">\n'
 
         if 'characteristics' in animal:
             if 'diet' in animal['characteristics']:
-                output += f"    Diet: {animal['characteristics']['diet']}<br/>\n"
+                output += f'      <strong>Diet:</strong> {animal["characteristics"]["diet"]}<br/>\n'
 
             if 'locations' in animal and len(animal['locations']) > 0:
-                output += f"    Location: {animal['locations'][0]}<br/>\n"
+                output += f'      <strong>Location:</strong> {animal["locations"][0]}<br/>\n'
 
             if 'type' in animal['characteristics']:
-                output += f"    Type: {animal['characteristics']['type']}<br/>\n"
+                output += f'      <strong>Type:</strong> {animal["characteristics"]["type"]}<br/>\n'
 
+        output += '  </p>\n'
         output += '</li>\n'
 
     with open("animal_template.html", "r") as f:
         template_content = f.read()
 
-    new_html_content = template_content.replace("__REPLACE_ANIMALS_INFO__", output)
+    final_html = template_content.replace("__REPLACE_ANIMALS_INFO__", output)
 
     with open("animals.html", "w") as f:
-        f.write(new_html_content)
+        f.write(final_html)
 
-    print("Schritt 3 abgeschlossen: 'animals.html' mit HTML-Tags generiert.")
+    print("Glückwunsch! Schritt 4 abgeschlossen. 'animals.html' ist nun fertig gestaltet.")
 
 
 if __name__ == "__main__":
