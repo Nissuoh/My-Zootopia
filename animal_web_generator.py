42f07a562a0400b1d2a4ec27f832151e0decf540
Houssin <el-hb@outlook.de>
2025-12-24 20:51:03 +0100

    verbessere HTML der Karten-Elemente

--- animal_web_generator.py
+++ animal_web_generator.py
@@ -6,40 +6,46 @@ def load_data(file_path):
         return json.load(handle)
 
 
+def serialize_animal(animal_obj):
+    output = '<li class="cards__item">\n'
+
+    if 'name' in animal_obj:
+        output += f'  <div class="card__title">{animal_obj["name"]}</div>\n'
+
+    output += '  <p class="card__text">\n'
+
+    characteristics = animal_obj.get('characteristics', {})
+
+    if 'diet' in characteristics:
+        output += f'      <strong>Diet:</strong> {characteristics["diet"]}<br/>\n'
+
+    if 'locations' in animal_obj and len(animal_obj['locations']) > 0:
+        output += f'      <strong>Location:</strong> {animal_obj["locations"][0]}<br/>\n'
+
+    if 'type' in characteristics:
+        output += f'      <strong>Type:</strong> {characteristics["type"]}<br/>\n'
+
+    output += '  </p>\n'
+    output += '</li>\n'
+    return output
+
+
 def main():
     animals_data = load_data('animal_data.json')
 
-    output = ''
-    for animal in animals_data:
-        output += '<li class="cards__item">\n'
-
-        if 'name' in animal:
-            output += f'  <div class="card__title">{animal["name"]}</div>\n'
-
-        output += '  <p class="card__text">\n'
-
-        if 'characteristics' in animal:
-            if 'diet' in animal['characteristics']:
-                output += f'      <strong>Diet:</strong> {animal["characteristics"]["diet"]}<br/>\n'
-
-            if 'locations' in animal and len(animal['locations']) > 0:
-                output += f'      <strong>Location:</strong> {animal["locations"][0]}<br/>\n'
-
-            if 'type' in animal['characteristics']:
-                output += f'      <strong>Type:</strong> {animal["characteristics"]["type"]}<br/>\n'
-
-        output += '  </p>\n'
-        output += '</li>\n'
+    animals_html = ''
+    for animal_obj in animals_data:
+        animals_html += serialize_animal(animal_obj)
 
     with open("animal_template.html", "r") as f:
         template_content = f.read()
 
-    final_html = template_content.replace("__REPLACE_ANIMALS_INFO__", output)
+    final_html = template_content.replace("__REPLACE_ANIMALS_INFO__", animals_html)
 
     with open("animals.html", "w") as f:
         f.write(final_html)
 
-    print("Glückwunsch! Schritt 4 abgeschlossen. 'animals.html' ist nun fertig gestaltet.")
+    print("Projekt erfolgreich abgeschlossen: 'animals.html' wurde generiert.")
 
 
 if __name__ == "__main__":
